5c6413017eda0526fc65f8b97a35e108c040ef9b
Brad Scott <bscott22@mac.com>
2023-08-22 12:20:09 -0400

    Initial Commit

---
+++ SparkScan/SparkScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkScanApp.swift
+//  SparkScan
+//
+//  Created by Brad Scott on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SparkScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
